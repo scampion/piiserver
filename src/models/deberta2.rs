94478fafb849ae59d76b1dff25ea12424193e10d
Sebastien Campion <sebastien.campion@gmail.com>
2025-01-25 12:44:31 +0100

    feat: implement DeBERTa v2 model with embeddings, attention, and encoder layers

--- src/models/deberta2.rs
+++ src/models/deberta2.rs
@@ -138,3 +138,367 @@ fn layer_norm(size: usize, eps: f64, vb: VarBuilder) -> Result<LayerNorm> {
     Ok(LayerNorm::new(weight, bias, eps))
 }
 
+pub struct DebertaV2Embeddings {
+    word_embeddings: Embedding,
+    position_embeddings: Option<Embedding>,
+    token_type_embeddings: Embedding,
+    layer_norm: LayerNorm,
+    dropout: Dropout,
+    pub padding_idx: u32,
+}
+
+impl DebertaV2Embeddings {
+    pub fn load(vb: VarBuilder, config: &DebertaV2Config) -> Result<Self> {
+        let word_embeddings = embedding(
+            config.vocab_size,
+            config.hidden_size,
+            vb.pp("word_embeddings"),
+        )?;
+        let position_embeddings = if config.position_biased_input {
+            Some(embedding(
+                config.max_position_embeddings,
+                config.hidden_size,
+                vb.pp("position_embeddings"),
+            )?)
+        } else {
+            None
+        };
+        let token_type_embeddings = embedding(
+            config.type_vocab_size,
+            config.hidden_size,
+            vb.pp("token_type_embeddings"),
+        )?;
+        let layer_norm = layer_norm(
+            config.hidden_size,
+            config.layer_norm_eps,
+            vb.pp("LayerNorm"),
+        )?;
+        let padding_idx = config.pad_token_id as u32;
+
+        Ok(Self {
+            word_embeddings,
+            position_embeddings,
+            token_type_embeddings,
+            layer_norm,
+            dropout: Dropout::new(config.hidden_dropout_prob),
+            padding_idx,
+        })
+    }
+
+    pub fn forward(
+        &self,
+        input_ids: &Tensor,
+        token_type_ids: &Tensor,
+        position_ids: Option<&Tensor>,
+        inputs_embeds: Option<&Tensor>,
+    ) -> Result<Tensor> {
+        let position_ids = match position_ids {
+            Some(ids) => ids.to_owned(),
+            None => {
+                if Option::is_some(&inputs_embeds) {
+                    let position_ids =
+                        self.create_position_ids_from_input_embeds(inputs_embeds.unwrap())?;
+                    position_ids
+                } else {
+                    let position_ids =
+                        create_position_ids_from_input_ids(input_ids, self.padding_idx, 1)?;
+                    position_ids
+                }
+            }
+        };
+
+        let inputs_embeds: Tensor = match inputs_embeds {
+            Some(embeds) => embeds.to_owned(),
+            None => {
+                let embeds = self.word_embeddings.forward(input_ids)?;
+                embeds
+            }
+        };
+
+        let token_type_embeddings = self.token_type_embeddings.forward(token_type_ids)?;
+        let mut embeddings = (inputs_embeds + token_type_embeddings)?;
+
+        if let Some(position_embeddings) = &self.position_embeddings {
+            embeddings = embeddings.broadcast_add(&position_embeddings.forward(&position_ids)?)?
+        }
+
+        let embeddings = self.layer_norm.forward(&embeddings)?;
+        let embeddings = self.dropout.forward(&embeddings)?;
+
+        Ok(embeddings)
+    }
+
+    pub fn create_position_ids_from_input_embeds(&self, input_embeds: &Tensor) -> Result<Tensor> {
+        let input_shape = input_embeds.dims3()?;
+        let seq_length = input_shape.1;
+
+        let mut position_ids = Tensor::arange(
+            self.padding_idx + 1,
+            seq_length as u32 + self.padding_idx + 1,
+            &Device::Cpu,
+        )?;
+
+        position_ids = position_ids
+            .unsqueeze(0)?
+            .expand((input_shape.0, input_shape.1))?;
+        Ok(position_ids)
+    }
+}
+
+struct DebertaV2SelfAttention {
+    query: Linear,
+    key: Linear,
+    value: Linear,
+    dropout: Dropout,
+    num_attention_heads: usize,
+    attention_head_size: usize,
+    relative_attention: bool,
+    max_relative_positions: i32,
+}
+
+impl DebertaV2SelfAttention {
+    fn load(vb: VarBuilder, config: &DebertaV2Config) -> Result<Self> {
+        let attention_head_size = config.hidden_size / config.num_attention_heads;
+        let all_head_size = config.num_attention_heads * attention_head_size;
+        let dropout = Dropout::new(config.attention_probs_dropout_prob);
+        let hidden_size = config.hidden_size;
+        let query = linear(hidden_size, all_head_size, vb.pp("query"))?;
+        let value = linear(hidden_size, all_head_size, vb.pp("value"))?;
+        let key = linear(hidden_size, all_head_size, vb.pp("key"))?;
+        Ok(Self {
+            query,
+            key,
+            value,
+            dropout,
+            num_attention_heads: config.num_attention_heads,
+            attention_head_size,
+            relative_attention: config.relative_attention,
+            max_relative_positions: config.max_relative_positions,
+        })
+    }
+
+    fn transpose_for_scores(&self, xs: &Tensor) -> Result<Tensor> {
+        let mut new_x_shape = xs.dims().to_vec();
+        new_x_shape.pop();
+        new_x_shape.push(self.num_attention_heads);
+        new_x_shape.push(self.attention_head_size);
+        let xs = xs.reshape(new_x_shape.as_slice())?.transpose(1, 2)?;
+        xs.contiguous()
+    }
+
+    fn forward(&self, hidden_states: &Tensor) -> Result<Tensor> {
+        let query_layer = self.query.forward(hidden_states)?;
+        let key_layer = self.key.forward(hidden_states)?;
+        let value_layer = self.value.forward(hidden_states)?;
+
+        let query_layer = self.transpose_for_scores(&query_layer)?;
+        let key_layer = self.transpose_for_scores(&key_layer)?;
+        let value_layer = self.transpose_for_scores(&value_layer)?;
+
+        let attention_scores = query_layer.matmul(&key_layer.t()?)?;
+        let attention_scores = (attention_scores / (self.attention_head_size as f64).sqrt())?;
+        let attention_probs =
+            { candle_nn::ops::softmax(&attention_scores, candle_core::D::Minus1)? };
+        let attention_probs = self.dropout.forward(&attention_probs)?;
+
+        let context_layer = attention_probs.matmul(&value_layer)?;
+        let context_layer = context_layer.transpose(1, 2)?.contiguous()?;
+        let context_layer = context_layer.flatten_from(candle_core::D::Minus2)?;
+        Ok(context_layer)
+    }
+}
+
+struct DebertaV2SelfOutput {
+    dense: Linear,
+    layer_norm: LayerNorm,
+    dropout: Dropout,
+}
+
+impl DebertaV2SelfOutput {
+    fn load(vb: VarBuilder, config: &DebertaV2Config) -> Result<Self> {
+        let dense = linear(config.hidden_size, config.hidden_size, vb.pp("dense"))?;
+        let layer_norm = layer_norm(
+            config.hidden_size,
+            config.layer_norm_eps,
+            vb.pp("LayerNorm"),
+        )?;
+        let dropout = Dropout::new(config.hidden_dropout_prob);
+        Ok(Self {
+            dense,
+            layer_norm,
+            dropout,
+        })
+    }
+
+    fn forward(&self, hidden_states: &Tensor, input_tensor: &Tensor) -> Result<Tensor> {
+        let hidden_states = self.dense.forward(hidden_states)?;
+        let hidden_states = self.dropout.forward(&hidden_states)?;
+        self.layer_norm.forward(&(hidden_states + input_tensor)?)
+    }
+}
+
+struct DebertaV2Attention {
+    self_attention: DebertaV2SelfAttention,
+    self_output: DebertaV2SelfOutput,
+}
+
+impl DebertaV2Attention {
+    fn load(vb: VarBuilder, config: &DebertaV2Config) -> Result<Self> {
+        let self_attention = DebertaV2SelfAttention::load(vb.pp("self"), config)?;
+        let self_output = DebertaV2SelfOutput::load(vb.pp("output"), config)?;
+        Ok(Self {
+            self_attention,
+            self_output,
+        })
+    }
+
+    fn forward(&self, hidden_states: &Tensor) -> Result<Tensor> {
+        let self_outputs = self.self_attention.forward(hidden_states)?;
+        let attention_output = self.self_output.forward(&self_outputs, hidden_states)?;
+        Ok(attention_output)
+    }
+}
+
+struct DebertaV2Intermediate {
+    dense: Linear,
+    intermediate_act: HiddenActLayer,
+}
+
+impl DebertaV2Intermediate {
+    fn load(vb: VarBuilder, config: &DebertaV2Config) -> Result<Self> {
+        let dense = linear(config.hidden_size, config.intermediate_size, vb.pp("dense"))?;
+        Ok(Self {
+            dense,
+            intermediate_act: HiddenActLayer::new(&config.hidden_act),
+        })
+    }
+
+    fn forward(&self, hidden_states: &Tensor) -> Result<Tensor> {
+        let hidden_states = self.dense.forward(hidden_states)?;
+        let ys = self.intermediate_act.forward(&hidden_states)?;
+        Ok(ys)
+    }
+}
+
+struct DebertaV2Output {
+    dense: Linear,
+    layer_norm: LayerNorm,
+    dropout: Dropout,
+}
+
+impl DebertaV2Output {
+    fn load(vb: VarBuilder, config: &DebertaV2Config) -> Result<Self> {
+        let dense = linear(config.intermediate_size, config.hidden_size, vb.pp("dense"))?;
+        let layer_norm = layer_norm(
+            config.hidden_size,
+            config.layer_norm_eps,
+            vb.pp("LayerNorm"),
+        )?;
+        let dropout = Dropout::new(config.hidden_dropout_prob);
+        Ok(Self {
+            dense,
+            layer_norm,
+            dropout,
+        })
+    }
+
+    fn forward(&self, hidden_states: &Tensor, input_tensor: &Tensor) -> Result<Tensor> {
+        let hidden_states = self.dense.forward(hidden_states)?;
+        let hidden_states = self.dropout.forward(&hidden_states)?;
+        self.layer_norm.forward(&(hidden_states + input_tensor)?)
+    }
+}
+
+struct DebertaV2Layer {
+    attention: DebertaV2Attention,
+    intermediate: DebertaV2Intermediate,
+    output: DebertaV2Output,
+}
+
+impl DebertaV2Layer {
+    fn load(vb: VarBuilder, config: &DebertaV2Config) -> Result<Self> {
+        let attention = DebertaV2Attention::load(vb.pp("attention"), config)?;
+        let intermediate = DebertaV2Intermediate::load(vb.pp("intermediate"), config)?;
+        let output = DebertaV2Output::load(vb.pp("output"), config)?;
+        Ok(Self {
+            attention,
+            intermediate,
+            output,
+        })
+    }
+
+    fn forward(&self, hidden_states: &Tensor) -> Result<Tensor> {
+        let attention_output = self.attention.forward(hidden_states)?;
+        let intermediate_output = self.intermediate.forward(&attention_output)?;
+        let layer_output = self
+            .output
+            .forward(&intermediate_output, &attention_output)?;
+        Ok(layer_output)
+    }
+}
+
+struct DebertaV2Encoder {
+    layers: Vec<DebertaV2Layer>,
+}
+
+impl DebertaV2Encoder {
+    fn load(vb: VarBuilder, config: &DebertaV2Config) -> Result<Self> {
+        let layers = (0..config.num_hidden_layers)
+            .map(|index| DebertaV2Layer::load(vb.pp(&format!("layer.{index}")), config))
+            .collect::<Result<Vec<_>>>()?;
+        Ok(DebertaV2Encoder { layers })
+    }
+
+    fn forward(&self, hidden_states: &Tensor) -> Result<Tensor> {
+        let mut hidden_states = hidden_states.clone();
+        for layer in self.layers.iter() {
+            hidden_states = layer.forward(&hidden_states)?
+        }
+        Ok(hidden_states)
+    }
+}
+
+pub struct DebertaV2Model {
+    embeddings: DebertaV2Embeddings,
+    encoder: DebertaV2Encoder,
+    pub device: Device,
+}
+
+impl DebertaV2Model {
+    pub fn load(vb: VarBuilder, config: &DebertaV2Config) -> Result<Self> {
+        let (embeddings, encoder) = match (
+            DebertaV2Embeddings::load(vb.pp("embeddings"), config),
+            DebertaV2Encoder::load(vb.pp("encoder"), config),
+        ) {
+            (Ok(embeddings), Ok(encoder)) => (embeddings, encoder),
+            (Err(err), _) | (_, Err(err)) => {
+                if let Some(model_type) = &config.model_type {
+                    if let (Ok(embeddings), Ok(encoder)) = (
+                        DebertaV2Embeddings::load(vb.pp(&format!("{model_type}.embeddings")), config),
+                        DebertaV2Encoder::load(vb.pp(&format!("{model_type}.encoder")), config),
+                    ) {
+                        (embeddings, encoder)
+                    } else {
+                        return Err(err);
+                    }
+                } else {
+                    return Err(err);
+                }
+            }
+        };
+        Ok(Self {
+            embeddings,
+            encoder,
+            device: vb.device().clone(),
+        })
+    }
+
+    pub fn forward(&self, input_ids: &Tensor, token_type_ids: &Tensor) -> Result<Tensor> {
+        let embedding_output = self
+            .embeddings
+            .forward(input_ids, token_type_ids, None, None)?;
+        let sequence_output = self.encoder.forward(&embedding_output)?;
+        Ok(sequence_output)
+    }
+}
+
